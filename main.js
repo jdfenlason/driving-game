39b7c457602613d0ed6adb3dc105c75b3a3f6cea
Jake <jdfenlason@gmail.com>
2021-04-08 19:10:53 -0400

    rotate car on key down and create css classes to handle rotation

--- main.js
+++ main.js
@@ -3,21 +3,20 @@ var carNav = {
 };
 
 var $car = document.querySelector('#car');
-document.addEventListener('keydown', keyEvent);
+window.addEventListener('keydown', keyEvent);
 
 function keyEvent(event) {
   if (event.keyCode === 37) {
-    $car.className = 'rotate-180-left';
+    $car.className = 'left';
     carNav.direction = 'west';
   } else if (event.keyCode === 38) {
-    $car.className = 'rotate-90-left';
+    $car.className = 'up';
     carNav.direction = 'north';
   } else if (event.keyCode === 39) {
     $car.className = '';
     carNav.direction = 'east';
   } else if (event.keyCode === 40) {
-    $car.className = 'rotate-90-right';
+    $car.className = 'down';
     carNav.direction = 'south';
   }
-
 }
